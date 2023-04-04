3e5ecc54eae1555546ff6a6584f2ef992453f441
gogaokradze <deathofsuper@gmail.com>
2023-04-04 15:50:13 +0400

    fix

--- accounts/accounts.js
+++ accounts/accounts.js
@@ -2,31 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 const aggregatePaginate = require("mongoose-aggregate-paginate-v2");
 
-// const accounts = new mongoose.Schema(
-//   {
-//     address: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//     },
-//     balance: Number,
-//     account_category: String,
-//     account_type_id: { type: Schema.Types.ObjectId, ref: "account_types" },
-//     account_owner: {
-//       type: String,
-//     },
-//     active: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     staked: [],
-//   },
-//   {
-//     timestamps: true,
-//   },
-// );
-
-const eventSchema = new mongoose.Schema(
+const accounts = new mongoose.Schema(
   {
     address: {
       type: String,
@@ -47,42 +23,21 @@ const eventSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-    discriminatorKey: "account_category",
   },
 );
 
-const Event = mongoose.model("Event", eventSchema);
+accounts.pre("save", function (next) {
+  if (this.account_category === "external") {
+    this.extensions = {
+      trade: false,
+      loan: false,
+      notify: false,
+      staking: false,
+      referral: false,
+    };
+  }
+  next();
+});
 
-const externalTypeEvent = Event.discriminator(
-  "external",
-  new Schema({
-    extensions: {
-      trade: {
-        type: Boolean,
-        default: false,
-      },
-      loan: {
-        type: Boolean,
-        default: false,
-      },
-      notify: {
-        type: Boolean,
-        default: false,
-      },
-      staking: {
-        type: Boolean,
-        default: false,
-      },
-      referral: {
-        type: Boolean,
-        default: false,
-      },
-      connect: {
-        type: Boolean,
-        default: process.env.REACT_APP_CONNECT === "true",
-      },
-    },
-  }),
-);
-
-module.exports = mongoose.models.accounts || mongoose.model("accounts", Event);
+accounts.plugin(aggregatePaginate);
+module.exports = mongoose.models.accounts || mongoose.model("accounts", accounts);
