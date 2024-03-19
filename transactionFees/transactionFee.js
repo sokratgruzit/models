ca4b5e5508af33d3850faa3c5672d54b8ccc7766
sibel <pipiaalex7@gmail.com>
2024-03-20 02:43:48 +0400

    f46

---
+++ transactionFees/transactionFee.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const transactionFeeSchema = new mongoose.Schema (
+
+    {
+        TransactionType: {
+            type: String,
+            required: true,
+        },
+          Description: {
+            type: String,
+            required: true,
+        },
+          FeeType: {
+            type: String,
+            required: true,
+        },
+          BaseFee: {
+            type: String,
+            required: true,
+        },
+          FixedFee: {
+            type: String,
+            required: true,
+        },
+          VariableFeeRate: {
+            type: String,
+            required: true,
+        },
+          FeeCurrency: {
+            type: String,
+            required: true,
+        },
+          BaseFeeInBaseCurrency: {
+            type: String,
+            required: true,
+        },
+          BaseCurrency: {
+            type: String,
+            required: true,
+        },
+          MinFeeInBaseCurrency: {
+            type: String,
+            required: true,
+        },
+          MaxFeeInBaseCurrency: {
+            type: String,
+            required: true,
+        },
+          IsOnChain: {
+            type: String,
+            required: true,
+        },
+          Network: {
+            type: String,
+            required: true,
+        }, 
+         GasLimit: {
+            type: String,
+            required: true,
+        },
+          LastUpdated: {
+            type: String,
+            required: true,
+        }
+    }
+);
+
+module.exports = mongoose.models.transactionFee || mongoose.model("transactionFee", transactionFeeSchema);
